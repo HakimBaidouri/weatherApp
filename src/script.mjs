3a295057159821482f067c3be74f612722997b7c
Hakim Baidouri <hakim.baidouri@hotmail.com>
2024-11-28 14:20:41 +0100

    CSS added

--- src/script.mjs
+++ src/script.mjs
@@ -1,17 +1,18 @@
 import { getCityList } from '../city.mjs';
 import { getCityData } from '../weather.mjs';
 import { getPhotoUrl } from '../photo.mjs';
-
 import { Chart, registerables } from 'chart.js';
 
 const datalist = document.querySelector("datalist");
 const input = document.querySelector("input");
 const submit = document.querySelector("button");
 const refreshButton = document.getElementById("search-refresh");
+
 const container = document.createElement("div");
 container.className = "container";
 container.style.display = "flex";
 container.style.gap = "25px";
+container.style.flexDirection = "column";
 document.body.appendChild(container);
 
 let choice = "";
@@ -50,14 +51,20 @@ submit.addEventListener("click",async (event) => {
 
     const dataList = cityData.list;
 
-    //Creation de la div weather et d'un enfant h2 = à la ville sélectionnée
-    const weatherDiv = document.createElement("div");
-    weatherDiv.className = "weather";
-    container.appendChild(weatherDiv);
     const h2 = document.createElement("h2");
     h2.className = "city";
     h2.textContent = choice;
-    weatherDiv.appendChild(h2);
+    container.appendChild(h2);
+
+    //Creation de la div weather-today et wearther-other-day
+    const weatherDiv = document.createElement("div");
+    weatherDiv.className = "weather-today";
+    container.appendChild(weatherDiv);
+
+    const weatherOtherDayDiv = document.createElement("div");
+    weatherOtherDayDiv.className = "weather-other-day";
+    weatherOtherDayDiv.style.display = "flex";
+    container.appendChild(weatherOtherDayDiv);
 
 
     //On cherche le première index de Json.list où l'heure est à minuit pour savoir où le jour 1 se termine
@@ -110,49 +117,23 @@ submit.addEventListener("click",async (event) => {
 
     //On affiche les min/max de chaque journée en indiquant le jour correspondant
     const currentDate = new Date();
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const daysOfWeek = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY','SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
 
     
     for(let count = 0; count < minArr.length; count++){
+
         const div = document.createElement("div");
-        if(count === 0){
-        div.className = "Today";
-        const h3 = document.createElement("h3");
-        h3.textContent = div.className;
-        const p_minTemp = document.createElement("p");
-        p_minTemp.textContent = "Minimum temperature : " + Math.round(minArr[count]).toString() + " °C";
-        const p_maxTemp = document.createElement("p");
-        p_maxTemp.textContent = "Maximum temperature : " + Math.round(maxArr[count]).toString() + " °C";
-        div.appendChild(h3);
-        div.appendChild(p_minTemp);
-        div.appendChild(p_maxTemp);
-        } else {
-        div.className = daysOfWeek[currentDate.getDay()+ count];
-        const h3 = document.createElement("h3");
-        h3.textContent = div.className;
-        const p_minTemp = document.createElement("p");
-        p_minTemp.textContent = "Min temp : " + Math.round(minArr[count]).toString() + " °C";
-        const p_maxTemp = document.createElement("p");
-        p_maxTemp.textContent = "Max temp : " + Math.round(maxArr[count]).toString() + " °C";
-        div.appendChild(h3);
-        div.appendChild(p_minTemp);
-        div.appendChild(p_maxTemp);
-        }
-
-        weatherDiv.appendChild(div);
-
         Chart.register(...registerables);
-
+        
         // Créez dynamiquement un élément canvas avec des dimensions spécifiques
         const canvas = document.createElement('canvas');
         canvas.id = 'myChart'; // Attribuez un ID pour le contexte 2D
-        canvas.width = 100; // Largeur du canvas
-        canvas.height = 100; // Hauteur du canvas
-        weatherDiv.appendChild(canvas); // Ajoutez le canvas à la div
-
-        // Obtenez le contexte 2D du canvas
-        const ctx = canvas.getContext('2d');
-
+        // canvas.width = 500; // Largeur du canvas
+        // canvas.height = 600; // Hauteur du canvas
+        canvas.style.width = "90%";
+        // canvas.style.maxWidth = "90%";
+        canvas.style.height = "500px";
+        
         let arrHours = [];
         let arrTemps = [];
         
@@ -160,42 +141,93 @@ submit.addEventListener("click",async (event) => {
             arrHours.push(dataList[count2].dt_txt.split(" ")[1]);
             arrTemps.push(dataList[count2].main.temp);
         }
+        
+        if(count === 0){
+            div.className = "TODAY";
+            const h3 = document.createElement("h3");
+            h3.textContent = div.className;
+            const p_minTemp = document.createElement("p");
+            p_minTemp.textContent = "Minimum temperature : " + Math.round(minArr[count]).toString() + " °C";
+            const p_maxTemp = document.createElement("p");
+            p_maxTemp.textContent = "Maximum temperature : " + Math.round(maxArr[count]).toString() + " °C";
+            p_maxTemp.style.color = "#e57272"
+            div.appendChild(h3);
+            div.appendChild(p_minTemp);
+            div.appendChild(p_maxTemp);
 
+            weatherDiv.appendChild(div);
+            div.appendChild(canvas); // Ajoutez le canvas à la div
+        } else {
+            div.className = "other-day";
+            div.id = daysOfWeek[currentDate.getDay()+ count];
+            const h3 = document.createElement("h3");
+            h3.textContent = div.id;
+            const p_minTemp = document.createElement("p");
+            p_minTemp.textContent = "Min temp : " + Math.round(minArr[count]).toString() + " °C";
+            const p_maxTemp = document.createElement("p");
+            p_maxTemp.textContent = "Max temp : " + Math.round(maxArr[count]).toString() + " °C";
+            p_maxTemp.style.color = "#e57272"
+            div.appendChild(h3);
+            div.appendChild(p_minTemp);
+            div.appendChild(p_maxTemp);
+
+            weatherOtherDayDiv.appendChild(div);
+            div.appendChild(canvas); // Ajoutez le canvas à la div
+        }
+        
+        // Obtenez le contexte 2D du canvas
+        const ctx = canvas.getContext('2d');
 
         const myChart = new Chart(ctx, {
-            type: 'line', // Type du graphique
+            type: 'line',
             data: {
-                labels: arrHours,
+                labels: arrHours, // Les labels pour les heures
                 datasets: [{
                     label: 'Temperature',
-                    data: arrTemps,
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                    data: arrTemps, // Les données pour les températures
+                    borderColor: '#FCB72B', // Couleur de la ligne
+                    borderWidth: 3
                 }]
             },
             options: {
                 scales: {
+                    x: {
+                        ticks: {
+                            color: '#E5ECF4' // Couleur des ticks (texte des labels sur l'axe x)
+                        },
+                        grid: {
+                            color: '#E5ECF4' // Couleur des lignes de la grille de l'axe x
+                        }
+                    },
                     y: {
-                        beginAtZero: true // L'échelle commence à 0
+                        beginAtZero: true,
+                        ticks: {
+                            color: '#E5ECF4' // Couleur des ticks (texte des labels sur l'axe y)
+                        },
+                        grid: {
+                            color: '#E5ECF4' // Couleur des lignes de la grille de l'axe y
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: '#E5ECF4' // Couleur du texte de la légende
+                        }
                     }
                 }
             }
         });
     }
 
-    const img = document.createElement("img");
+    // const img = document.createElement("img");
     const imgUrl = await getPhotoUrl(choice);
-    img.src = imgUrl;
-    console.log(imgUrl);
-    img.alt = "This is an image of " + choice;
-    weatherDiv.appendChild(img);
+    // img.src = imgUrl;
+    // console.log(imgUrl);
+    // img.alt = "This is an image of " + choice;
+    // container.appendChild(img);
+    const body = document.body;
+    body.style.backgroundImage = "url(" + imgUrl +")";
 })
 
 refreshButton.addEventListener("click", () => {
